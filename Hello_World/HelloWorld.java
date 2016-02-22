0928837a633da4c618b545704954f9a2378a2afa
iHht <hht_299@hotmail.com>
2016-02-22 14:06:08 -0800

    HelloWorld

---
+++ Hello_World/HelloWorld.java
@@ -0,0 +1,18 @@
+/**
+ * @(#)HelloWorld.java
+ *
+ *
+ * @author Habeeb Altalag
+ * @version 1.00 2016/2/22
+ */
+
+
+public class HelloWorld {
+
+    public static void main (String[] args)
+    {
+    	System.out.println("Hello World!");
+    }
+    
+    
+}
